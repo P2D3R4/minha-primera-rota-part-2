1041628c478d08d68cf9188020735f892dd6b9b0
Pedro Martos <54295971+P2D3R4@users.noreply.github.com>
2022-05-09 22:28:40 +0000

    Final 2

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ app = Flask('app')
 
 @app.route('/')
 def unifran():
-  return "<h1>Minha Primeira Rota!</h1>'"
+  return '<h1>Minha Primeira Rota!</h1>'
      
 @app.route('/unifran')
 def unifran2():
-       return "<h2> Universidade de Franca </h2>"
+  return '<h2> Universidade de Franca </h2>'
 
 if __name__=='__main__':     
   app.run(host='0.0.0.0', port=8080)
